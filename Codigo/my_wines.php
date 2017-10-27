4c814bdbcfefc3354293e8bf9b5d48d3153cf439
Victor Rodrigues da Silva <vitor1908@gmail.com>
2017-10-27 20:20:45 -0200

    Codigo tela my_wine

---
+++ Codigo/my_wines.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Meus Vinhos</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="tudo">
+		<div class="container-fluid">
+			<div class="row espacamento-interno-menu">
+				<div class="col-8 separador">
+					<a href="index.php"><img src="logo.png" class="img-fluid" alt="Home" style="height: 30px; margin-left: 10px"></a>
+				</div>
+				<div class="col-4">
+					<ul class="nav">
+						<li class="nav-item">
+							<a class="nav-link active" href="index.php"  style="font-weight: bold;">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="index.php">Explorar</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Cadastrar</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Entrar</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		
+		<div class="container-fluid fundo">
+			<div class="row">
+				<div class="col-sm-12">	
+
+					
+
+					<div class="row justify-content-center sobreposta" >
+						<div class="col-sm-4 ml-sm-auto esquerda_foto">
+							<div class="row justify-content-center ">
+								<div class="fotoUser">
+									<img src="user.jpg">
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-7 mr-sm-auto direita_vinhos">
+							<nav class="navbar fixed-top navbar-expand-md bg-light botao">						
+								<div class="collapse navbar-collapse">
+									<ul class="navbar-nav">								
+										<li class="nav-item">
+											<a class="nav-link" href="#">CADASTRAR VINHO</a>
+										</li>
+										
+									</ul>
+								</div>
+							</nav>
+
+						</div>
+					</div>
+				</div>				
+			</div>
+		</div>
+
+		<div class="footer-bottom">
+			<div class="container">
+				<footer class="footer">
+					<div class="row justify-content-center">
+						<div class="copyright">
+							© 2017, Vovoni, All rights reserved
+						</div>
+					</div>
+					
+				</footer>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
